aee78a8bbce0b41e88ade909511855e24a71d28e
Adrien <adrien.colombier@epitech.eu>
2022-07-26 00:07:13 +0200

    [EDIT] - Prepare register page

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/User.model';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  passwordVisibility: string = 'password';
+  warningMessage: string = '';
+  user: User = new User();
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  togglePasswordVisibility() {
+    this.passwordVisibility = this.passwordVisibility === 'password' ? 'text' : 'password';
+  }
+
+  checkIfInputsAreValid(): boolean {
+    if (this.user.firstName === '') {
+      this.warningMessage = 'Entrez votre prénom *';
+      return false;
+    }
+    if (this.user.lastName === '') {
+      this.warningMessage = 'Entrez votre nom *';
+      return false;
+    }
+    if (this.user.email === '') {
+      this.warningMessage = 'Entrez une adresse email valide *';
+      return false;
+    }
+    if (this.user.password === '') {
+      this.warningMessage = 'Entrez un mot de passe *';
+      return false;
+    }
+    this.warningMessage = '';
+    return (true);
+  }
+
+  connectUser() {
+    let isInputValid: boolean = false;
+
+    isInputValid = this.checkIfInputsAreValid();
+    if (isInputValid) {
+      console.log('Connecting user...');
+    }
+  }
 }
